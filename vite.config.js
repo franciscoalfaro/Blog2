15511b23408396818bc2e25db57d3a16d3512da5
Francisco alfaro <franciscoalfar@gmail.com>
2024-04-25 01:22:37 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/' // Ruta base relativa al dominio raíz
-});
+  base:'https://franciscoalfaro.github.io/'
+})
